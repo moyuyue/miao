fd8c9955aaec2c7f712e2c657a5aaf68eb02c039
moyuyue <761660113@qq.com>
2019-01-27 22:46:52 +0800

    first commit

--- lodash/moyuyue-lodash.js
+++ lodash/moyuyue-lodash.js
@@ -3,14 +3,31 @@
 var moyuyue = function(){
 
   function chunk(ary, size = 1) {
+  	if (ary.length == 0) return []
 
+  	var result = []
+  	var a = ary.slice(0,size);
+  	var b = ary.slice(size);
+
+  	result.push(a,b)
+  	return result
   }
-  function compact(ary) {
 
+
+  function compact(ary) {
+  	//数组过滤
+  	return ary.filter(item => item)
+  }
+
+  function difference(ary) {
+  	var a = ary[0];
+  	var b = [].concat(...ary.slice(1))
+  	return a.filter(item => b.indexOf(item) != -1)
   }
 
   return {
     chunk: chunk,
     compact: compact,
+    difference: difference,
   }
 }()
