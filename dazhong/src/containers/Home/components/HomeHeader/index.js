51778d90c5958c18936ab7a3ffad10d90d31c90b
moyuyue <761660113@qq.com>
2019-06-12 15:10:16 +0800

    first commit

---
+++ dazhong/src/containers/Home/components/HomeHeader/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import "./style.css"
+
+import { Link } from 'react-router-dom' 
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <div className="homeHeader">
+        <header className="homeHeader__wrapper">
+          <span className="homeHeader__city">北京</span>
+          <Link to="/search" className="homeHeader__search">输入商户名、地点</Link>
+          <Link to="/user" className="homeHeader__self">
+            <div className="homeHeader__portrait"/>
+          </Link>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default HomeHeader;
